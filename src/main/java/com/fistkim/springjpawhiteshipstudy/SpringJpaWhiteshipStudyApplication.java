3b8272dfc9d2e17ed409ddd3752f73c254c1fd96
fistkim101 <fistkim101@gmail.com>
2022-12-14 19:14:35 +0900

    project init

---
+++ src/main/java/com/fistkim/springjpawhiteshipstudy/SpringJpaWhiteshipStudyApplication.java
@@ -0,0 +1,13 @@
+package com.fistkim.springjpawhiteshipstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaWhiteshipStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaWhiteshipStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fistkim/springjpawhiteshipstudy/SpringJpaWhiteshipStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fistkim.springjpawhiteshipstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaWhiteshipStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
