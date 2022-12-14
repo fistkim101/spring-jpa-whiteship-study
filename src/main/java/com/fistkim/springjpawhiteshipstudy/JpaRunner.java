ee7f62cae032f73ec00f234273cbece838c87681
fistkim101 <fistkim101@gmail.com>
2022-12-14 20:44:37 +0900

    entity manager

---
+++ src/main/java/com/fistkim/springjpawhiteshipstudy/JpaRunner.java
@@ -0,0 +1,25 @@
+package com.fistkim.springjpawhiteshipstudy;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Component
+public class JpaRunner implements ApplicationRunner {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public void run(ApplicationArguments args) throws Exception {
+        User user = new User();
+        user.setName("fistkim");
+        user.setPassword("password");
+        entityManager.persist(user);
+    }
+}
