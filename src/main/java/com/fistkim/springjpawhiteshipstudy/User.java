dced1608ed2d8104d1bff56969d50d2c00d96225
fistkim101 <fistkim101@gmail.com>
2022-12-14 20:41:42 +0900

    jpa sample

---
+++ src/main/java/com/fistkim/springjpawhiteshipstudy/User.java
@@ -0,0 +1,26 @@
+package com.fistkim.springjpawhiteshipstudy;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+}
