91c372db36920cf5b3258b3c7ec5a8f3e6aad508
fistkim101 <fistkim101@gmail.com>
2022-12-26 23:21:26 +0900

    entityManager

--- src/main/java/com/fistkim/springjpawhiteshipstudy/JpaRunner.java
+++ src/main/java/com/fistkim/springjpawhiteshipstudy/JpaRunner.java
@@ -1,30 +1,31 @@
 package com.fistkim.springjpawhiteshipstudy;
 
-import com.querydsl.core.types.Predicate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 
 @Component
 public class JpaRunner implements ApplicationRunner {
 
-    @Autowired
-    private PostRepository postRepository;
+    @PersistenceContext
+    private EntityManager entityManager1;
+
+    @PersistenceContext
+    private EntityManager entityManager2;
+
 
     @Override
     @Transactional
     public void run(ApplicationArguments args) throws Exception {
-        Post post = new Post();
-        post.setName("specificationPostName");
-        post.setDescription("specificationPostDescription");
-        postRepository.save(post);
-
-
+        System.out.println(">>>>>");
+        System.out.println(entityManager1);
+        System.out.println(entityManager2);
+        System.out.println("<<<<<");
     }
 
 }
